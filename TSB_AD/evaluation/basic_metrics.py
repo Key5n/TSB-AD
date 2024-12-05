fix: not warn ill-defined precision

TSB_AD/evaluation/basic_metrics.py
@@ -341,7 +341,7 @@ class basic_metricor():
                 tp = np.sum([preds[start:end + 1].any() for start, end in true_events.values()])
                 fn = len(true_events) - tp
                 rec_e = tp/(tp + fn)
-                prec_t = precision_score(label, preds)
+                prec_t = precision_score(label, preds, zero_division=0.0)
                 EventF1PA = 2 * rec_e * prec_t / (rec_e + prec_t + self.eps)
 
                 EventF1PA_scores.append(EventF1PA)