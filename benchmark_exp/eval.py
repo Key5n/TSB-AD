bd43ef7da39b60c290c6879c55aa1adf09ec48ca
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-12-08 13:45:33 +0900

    feat: make eval.py accept file location as argument

--- benchmark_exp/eval.py
+++ benchmark_exp/eval.py
@@ -1,9 +1,13 @@
 import csv
 import numpy as np
+import argparse
 
 VUS_PRs_against_HP: dict[str, list[float]] = {}
+parser = argparse.ArgumentParser(description="Evaluate hyper-parameters")
+parser.add_argument("--score_file", type=str, default="eval/HP_tuning/uni/MDRS.csv")
+args = parser.parse_args()
 
-with open("eval/HP_tuning/uni/MDRS.csv") as f:
+with open(args.score_file) as f:
     reader = csv.DictReader(f)
     for row in reader:
         HP = row["HP"]
